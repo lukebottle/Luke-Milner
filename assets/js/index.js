0d121d5ffb2c0de0e07e1411f7052606df11eafd
lukebottle <68571277+lukebottle@users.noreply.github.com>
2022-12-02 12:25:48 +0000

    content [update]: cors did not work :(

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 let fontColor = window.localStorage.getItem('font-color');
  //api url
  const apiKey = "EA5D5BCBBC33290E41E194D1EC5373D8",
-       corsProxy = "", // https://proxy.cors.sh/ use this in production. // https://cors-anywhere.herokuapp.com use this in local host.
+       corsProxy = "https://proxy.cors.sh/", // https://proxy.cors.sh/ use this in production. // https://cors-anywhere.herokuapp.com use this in local host.
  
  apiUrl = `${corsProxy}https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=76561198077564405`;
 
