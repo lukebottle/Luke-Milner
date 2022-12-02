28c6a9ba2f2212dfd169fb5a8b5b9d1502edf542
lukebottle <68571277+lukebottle@users.noreply.github.com>
2022-12-02 12:14:05 +0000

    content [update]: webpack (maybe cors fix)

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ let fontColor = window.localStorage.getItem('font-color');
  const apiKey = "EA5D5BCBBC33290E41E194D1EC5373D8",
        corsProxy = "", // https://proxy.cors.sh/ use this in production. // https://cors-anywhere.herokuapp.com use this in local host.
  
- apiUrl = `${corsProxy}https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=76561198077564405`;
+ apiUrl = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=76561198077564405`;
 
  // Defining async function
  async function getApi(url) {
